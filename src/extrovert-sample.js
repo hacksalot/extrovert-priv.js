af7b12e096b6ba51dada545267565a8559e49f5f
James Devlin <james@indevious.com>
2015-03-16 22:28:50 -0400

    Introduce sample generator.

---
+++ src/extrovert-sample.js
@@ -0,0 +1,56 @@
+/**
+A sample Extrovert generator for demonstration purposes.
+@module extrovert-gallery.js
+@copyright Copyright (c) 2015 by James M. Devlin
+@author James M. Devlin | james@indevious.com
+@license MIT
+@version 1.0
+*/
+
+(function (window, $, THREE, EXTROVERT) {
+
+
+
+   /**
+   Module object.
+   */
+   //var my = {};
+
+
+   /**
+   Default options.
+   */
+   var _def_opts = {
+      generator: {
+         name: 'sample',
+         background: 'default_background.png',
+         material: { color: 0x440000, friction: 0.2, restitution: 1.0 }
+      }
+   };
+
+
+   /**
+   @class The built-in 'sample' generator.
+   */
+   EXTROVERT.sample = function() {
+      return {
+         sample: function( options, eng ) {
+            var new_opts = $.extend(true, { }, _def_opts, options);
+            if( !new_opts.generator || typeof new_opts.generator == 'string' )
+               new_opts.generator = _def_opts.generator;
+            init_objects( new_opts, eng );
+         }
+      };
+   };
+
+
+
+   /**
+   Module return.
+   */
+   //return my;
+
+
+
+}(window, $, THREE, EXTROVERT));
+
