8fede0db317190c0f5b379d1011c3e16755f663a
James Devlin <james@indevious.com>
2015-04-04 10:13:13 -0400

    Add cookie support.

--- src/extrovert-utils.js
+++ src/extrovert-utils.js
@@ -370,6 +370,44 @@ EXTRO.Utils = (function (window, THREE) {
     return document[selectorType](selector);
   };
 
+  
+  
+  /*********************************************************
+  gets the value of a cookie
+  **********************************************************/
+  my.getCookie = function(sName)
+  {
+      sName = sName.toLowerCase();
+      var oCrumbles = document.cookie.split(';');
+      for(var i=0; i<oCrumbles.length;i++)
+      {
+          var oPair= oCrumbles[i].split('=');
+          var sKey = decodeURIComponent(oPair[0].trim().toLowerCase());
+          var sValue = oPair.length>1?oPair[1]:'';
+          if(sKey == sName)
+              return decodeURIComponent(sValue);
+      }
+      return '';
+  };
+  
+  /*********************************************************
+  sets the value of a cookie
+  **********************************************************/
+  my.setCookie = function(sName,sValue)
+  {
+      var oDate = new Date();
+      oDate.setYear(oDate.getFullYear()+1);
+      var sCookie = encodeURIComponent(sName) + '=' + encodeURIComponent(sValue) + ';expires=' + oDate.toGMTString() + ';path=/';
+      document.cookie = sCookie;
+  };
+  
+  /*********************************************************
+  removes the value of a cookie
+  **********************************************************/
+  my.clearCookie = function(sName)
+  {
+      my.setCookie(sName,'');
+  };
 
 
   /**
