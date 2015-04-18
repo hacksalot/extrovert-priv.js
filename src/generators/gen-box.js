44077f5e22b766865ba25cb9223ca2073c64d4d0
James Devlin <james@indevious.com>
2015-04-18 04:21:41 -0400

    Introduce box generator.

---
+++ src/generators/gen-box.js
@@ -0,0 +1,77 @@
+/**
+The built-in extrusion generator for Extrovert.js.
+@module gen-box.js
+@copyright Copyright (c) 2015 by James M. Devlin
+@author James M. Devlin | james@indevious.com
+@license MIT
+@version 1.0
+*/
+
+(function (window, EXTRO) {
+
+  EXTRO.box = function() {
+
+    var _opts, _eng, _side_mat, _noun;
+
+    return {
+
+      options: {
+        name: 'box',
+        material: { color: 0xFF8844, friction: 0.2, restitution: 1.0 },
+        block: { width: 250, height: 250, depth: 250 }
+      },
+
+      init: function( genOpts, eng ) {
+        _opts = genOpts;
+        _eng = eng;
+        _side_mat = EXTRO.createMaterial( genOpts.material );
+        EXTRO.createPlacementPlane( [ 0,0,0 ] );
+      },
+
+      generate: function( noun, elems ) {
+        _noun = noun;
+        var sides, obj, material, pos_info;
+        if( elems.length === 1 ) {
+          material = this.rasterize( elems[0] );
+          sides = [ material, material, material, material, material, material ];
+        }
+        else if( elems.length === 2 ) {
+          material = this.rasterize( elems[0] );
+          material2 = this.rasterize( elems[1] );
+          sides = [ _side_mat, _side_mat, _side_mat, _side_mat, material, material2 ];
+        }
+        else if (elems.length > 2) {
+          sides = [];
+          var length = elems.length > 6 ? 6 : elems.length;
+          for( var i = 0; i < length; i++ ) {
+            obj = elems[ i ];
+            material = this.rasterize( obj );
+            sides.push( material );
+          }
+          while( sides.length !== 6 )
+            sides.push( _side_mat );
+        }
+        
+        var mat_info = EXTRO.createCubeMaterial( sides );
+        EXTRO.createObject({ type: 'box', pos: [0,0,0], dims: [250,250,250], mat: mat_info, mass: 1000 });
+      },
+
+      rasterize: function( obj ) {
+        var rast = null;
+        if( _noun.rasterizer ) {
+          rast = ( typeof _noun.rasterizer === 'string' ) ?
+            new EXTRO['paint_' + _noun.rasterizer]() : _noun.rasterizer;
+        }
+        rast = rast || EXTRO.getRasterizer( obj );
+
+        var tileTexture = rast.paint(( _noun.adapt && _noun.adapt(obj) ) || obj );
+        var material = EXTRO.createMaterial({ tex: tileTexture, friction: 0.2, restitution: 1.0 });
+        return material;
+      },
+
+
+
+    };
+  };
+
+}(window, EXTRO));
