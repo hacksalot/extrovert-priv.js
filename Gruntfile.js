67d87434764c94528f62199d850bec44f35e33e1
James Devlin <james@indevious.com>
2015-04-18 22:57:52 -0400

    Remove gen-sample.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
       },
       dist: {
         sources: {
-          extro: ['src/extrovert.js', 'src/**/*.js', '!src/generators/gen-sample.js'],
+          extro: ['src/extrovert.js', 'src/**/*.js'],
           deps: ['bower_components/threejs/build/three.js', 'bower_components/physijs/physi.js'],
           merged: null /* filled at runtime */
         },
