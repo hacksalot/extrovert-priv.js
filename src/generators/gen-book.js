833dcb4c2ba50306033d6d60136d94a794c9d884
James Devlin <james@indevious.com>
2015-05-04 20:49:51 -0400

    Support double-sided book pages.

--- src/generators/gen-book.js
+++ src/generators/gen-book.js
@@ -15,9 +15,8 @@ A "book" generator for Extrovert.js.
 
     var _opts = null;
     var _eng = null;
-    var _side_mat = null;
+    var _side = null;
     var _noun = null;
-
 
 
 
@@ -25,20 +24,18 @@ A "book" generator for Extrovert.js.
     Adjust page textures mapping. Necessary because the page geometry is paper-
     back size (or user specified), but the page texture should be power-of-2 in
     both dimensions.
-    @method patch_textures
+    @method mapTextures
     */
     function mapTextures( cubeGeo ) {
-
       for (i = 0; i < cubeGeo.faces.length ; i++) {
-         var face = cubeGeo.faces[ i ];
-         var fvu = cubeGeo.faceVertexUvs[0][i];
-         if( Math.abs( face.normal.z ) > 0.9) {
-            for( var fv = 0; fv < 3; fv++ ) {
-              if( Math.abs( fvu[fv].y ) < 0.01 ) {
-                fvu[ fv ].y = 0.37;
-              }
+        var fvu = cubeGeo.faceVertexUvs[0][i];
+        if( Math.abs( cubeGeo.faces[ i ].normal.z ) > 0.9) {
+          for( var fv = 0; fv < 3; fv++ ) {
+            if( Math.abs( fvu[fv].y ) < 0.01 ) {
+              fvu[ fv ].y = 0.37;
             }
-         }
+          }
+        }
       }
       cubeGeo.uvsNeedUpdate = true;
     }
@@ -51,13 +48,14 @@ A "book" generator for Extrovert.js.
 
       options: {
         name: 'book',
-        material: { color: 0xFF8844, friction: 0.2, restitution: 1.0 },
+        material: { color: 0xCDCDCD, friction: 0.2, restitution: 1.0 },
         block: { depth: 'height' },
-        clickForce: 900000,
+        clickForce: 5000,
         rows: 10, cols: 10,
-        dims: [512, 814, 20], // Std paperback = 4.25 x 6.75 = 512x814
+        dims: [512, 814, 2], // Std paperback = 4.25 x 6.75 = 512x814
         pagify: true,
-        cover: null
+        cover: null,
+        doubleSided: true
       },
 
 
@@ -66,7 +64,7 @@ A "book" generator for Extrovert.js.
         _opts = genOpts;
         _eng = eng;
         extro.createPlacementPlane( [0,0,200] );
-        _side_mat = extro.createMaterial( genOpts.material );
+        _side = extro.createMaterial( genOpts.material );
       },
 
 
@@ -76,38 +74,51 @@ A "book" generator for Extrovert.js.
         extro.LOGGING && _eng.log.msg('book.generate( %o, %o )', noun, elems);
         _noun = noun;
 
+        if( noun.cover ) {
+          var coverMat = extro.createMaterial({ tex: extro.loadTexture( noun.cover ), friction: 0.2, resitution: 1.0 });
+          var coverMesh = extro.createObject({ type: 'box', pos: [0,0,0], dims: _opts.dims, mat: coverMat, mass: 1000 });
+        }
+
+        function _createMat( t ) { return extro.createMaterial({ tex: t, friction: 0.2, restitution: 1.0 }); }
+        function _isEven( val, index ) { return (index % 2) === 0; }
+        function _isOdd( val, index ) { return !_isEven(val, index); }
+
         for( var i = 0; i < elems.length; i++ ) {
 
-          var obj = elems[ i ];
-          var rast = null;
+          var obj = (noun.adapt && noun.adapt( elems[ i ] )) || elems[ i ];
 
+          var rast = null;
           if( noun.rasterizer ) {
-            if( typeof noun.rasterizer === 'string' )
-              rast = new extro['paint_' + noun.rasterizer]();
-            else
-              rast = noun.rasterizer;
-          }
-          else {
+            rast = ( typeof noun.rasterizer === 'string' ) ?
+              new extro['paint_' + noun.rasterizer]() : noun.rasterizer;
+          } else {
             rast = new extro.paint_plain_text_stream();
             //rast = extro.getRasterizer( obj );
           }
 
           if( _opts.pagify ) {
-            var done = false;
-            var info = { };
 
-            var tileTextures = rast.paint(( noun.adapt && noun.adapt(obj) ) || obj,
-              { width: _opts.texWidth || 512, // Force power-of-2 textures by default
+            var done = false,
+              info = { },
+              rastOpts = {
+                width: _opts.texWidth || 512, // Force power-of-2 textures
                 height: _opts.texHeight || 1024,
-                bkColor: _opts.bkColor,
-                textColor: _opts.textColor }, info );
+                bkColor: _opts.bkColor, textColor: _opts.textColor
+              },
+              textures = rast.paint(obj, rastOpts, info ),
+              matArray = [ _side, _side, _side, _side, null, null ];
 
-            for( tt = 0; tt < tileTextures.length; tt++ ) {
+            var mats = textures.map( _createMat );
+            var front = mats.filter( _isEven );
+            var back = mats.filter( _isOdd );
 
-            
-              var tilePos = [0, 0, -(tt * _opts.dims[2]) ];
-              var tileMat = extro.createMaterial({ tex: tileTextures[tt], friction: 0.2, restitution: 1.0 });
-              var mesh = extro.createObject({ type: 'box', pos: tilePos, dims: this.options.dims, mat: tileMat, mass: 1000 });
+            for( var tt = 0; tt < front.length; tt++ ) {
+
+              var tilePos = [0, 0, -(tt * _opts.dims[2]) - _opts.dims[2] ];
+              matArray[4] = front[ tt ];
+              matArray[5] = tt < back.length ? back[ tt ] : _side;
+              var meshMat = extro.createCubeMaterial( matArray );
+              var mesh = extro.createObject({ type: 'box', pos: tilePos, dims: _opts.dims, mat: meshMat, mass: 1000 });
               mapTextures( mesh.geometry );
               extro.LOGGING && _eng.log.msg('Generating page %o at position %f, %f, %f', mesh, tilePos[0], tilePos[1], tilePos[2]);
 
