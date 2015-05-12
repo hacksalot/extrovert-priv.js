3b50fcbe99e1adc12424da3518a12f7d0e01f6ec
James Devlin <james@indevious.com>
2015-05-12 00:54:05 -0400

    Continue.

--- src/extrovert/generators/gen-extrude.js
+++ src/extrovert/generators/gen-extrude.js
@@ -4,71 +4,77 @@ The built-in extrusion generator for Extrovert.js.
 @license Copyright (c) 2015 by James M. Devlin. All rights reserved.
 */
 
-define(['extrovert/core'], function( extrovert ) {
+define(['require', 'extrovert/core', 'extrovert/providers/three/provider-three'], function( require, extro, provider ) {
 
   'use strict';
 
   var _opts, _eng, _side_mat, _noun;
+  
+  var ExtrudeGenerator = function( ) {
+    
+    var extrovert = require('extrovert/core');    
 
-  return {
+    return {
 
-    options: {
-      name: 'extrude',
-      material: { color: 0xFF8844, friction: 0.2, restitution: 1.0 },
-      block: { depth: 1 },
-      map: 'fit'
-    },
+      options: {
+        name: 'extrude',
+        material: { color: 0xFF8844, friction: 0.2, restitution: 1.0 },
+        block: { depth: 1 },
+        map: 'fit'
+      },
 
-    init: function( genOpts, eng ) {
-      _opts = genOpts;
-      _eng = eng;
-      _side_mat = extrovert.provider.createMaterial( genOpts.material );
-      extrovert.createPlacementPlane( [ 0,0,0 ] );
-    },
+      init: function( genOpts, eng ) {
+        _opts = genOpts;
+        _eng = eng;
+        _side_mat = provider.createMaterial( genOpts.material );
+        extrovert.createPlacementPlane( [ 0,0,0 ] );
+      },
 
-    generate: function( noun, elems ) {
-      _noun = noun;
-      for( var i = 0; i < elems.length; i++ ) {
-        var obj = elems[ i ];
-        var pos_info = this.transform( obj );
-        var mat_info = this.rasterize( obj );
-        extrovert.createObject({ type: 'box', pos: pos_info.pos, dims: [pos_info.width, pos_info.height, pos_info.depth], mat: mat_info, mass: 1000 });
+      generate: function( noun, elems ) {
+        _noun = noun;
+        for( var i = 0; i < elems.length; i++ ) {
+          var obj = elems[ i ];
+          var pos_info = this.transform( obj );
+          var mat_info = this.rasterize( obj );
+          extrovert.createObject({ type: 'box', pos: pos_info.pos, dims: [pos_info.width, pos_info.height, pos_info.depth], mat: mat_info, mass: 1000 });
+        }
+      },
+
+      transform: function( obj ) {
+        var cont = _noun.container || (_eng.opts.src && _eng.opts.src.container) || document.body;
+        return extrovert.getPosition( obj, cont, _opts.block.depth );
+      },
+
+      rasterize: function( obj ) {
+        var rast = null;
+        if( _noun.rasterizer ) {
+          rast = ( typeof _noun.rasterizer === 'string' ) ?
+            _eng.rasterizers[ _noun.rasterizer ] : _noun.rasterizer;
+        }
+        rast = rast || extrovert.getRasterizer( obj );
+        var tileTexture = rast.paint(( _noun.adapt && _noun.adapt(obj) ) || obj );
+
+        var material = provider.createMaterial({ tex: tileTexture, friction: 0.2, restitution: 0.0 });
+
+        if( !_opts.map || _opts.map === 'all' ) {
+          return material;
+        }
+
+        var matArray;
+        if( _opts.map == 'fit' ) {
+          if( !_opts.block.depth || _opts.block.depth === 'height' )
+            matArray = [ _side_mat, _side_mat, material, material, material, material ];
+          else if (_opts.block.depth === 'width' )
+            matArray = [ material, material, _side_mat, _side_mat, material, material ];
+          else
+            matArray = [ _side_mat, _side_mat, _side_mat, _side_mat, material, material ];
+        }
+
+        return provider.createCubeMaterial( matArray );
       }
-    },
-
-    transform: function( obj ) {
-      var cont = _noun.container || (_eng.opts.src && _eng.opts.src.container) || document.body;
-      return extrovert.getPosition( obj, cont, _opts.block.depth );
-    },
-
-    rasterize: function( obj ) {
-      var rast = null;
-      if( _noun.rasterizer ) {
-        rast = ( typeof _noun.rasterizer === 'string' ) ?
-          new extrovert['paint_' + _noun.rasterizer]() : _noun.rasterizer;
-      }
-      rast = rast || extrovert.getRasterizer( obj );
-      var tileTexture = rast.paint(( _noun.adapt && _noun.adapt(obj) ) || obj );
-
-      var material = extrovert.provider.createMaterial({ tex: tileTexture, friction: 0.2, restitution: 0.0 });
-
-      if( !_opts.map || _opts.map === 'all' ) {
-        return material;
-      }
-
-      var matArray;
-      if( _opts.map == 'fit' ) {
-        if( !_opts.block.depth || _opts.block.depth === 'height' )
-          matArray = [ _side_mat, _side_mat, material, material, material, material ];
-        else if (_opts.block.depth === 'width' )
-          matArray = [ material, material, _side_mat, _side_mat, material, material ];
-        else
-          matArray = [ _side_mat, _side_mat, _side_mat, _side_mat, material, material ];
-      }
-
-      return extrovert.provider.createCubeMaterial( matArray );
-    }
-
+    };
   };
+  
+  return ExtrudeGenerator;
 
 });
--- src/extrovert/rasterizers/paint-element.js
+++ src/extrovert/rasterizers/paint-element.js
@@ -4,13 +4,12 @@ A simple Extrovert HTML rasterizer.
 @license Copyright (c) 2015 by James M. Devlin. All rights reserved.
 */
 
-define(['extrovert/core', 'extrovert/utils'], function( extrovert, Utils ) {
+define(['extrovert/providers/three/provider-three', 'extrovert/utils'], function( provider, utils ) {
 
   'use strict';
 
   return {
     paint: function( val, opts ) {
-      var _utils = Utils;
 
       // Create a canvas element. TODO: Reuse a single canvas.
       var canvas = document.createElement('canvas');
@@ -19,16 +18,15 @@ define(['extrovert/core', 'extrovert/utils'], function( extrovert, Utils ) {
       canvas.height = val.offsetHeight;
 
       // Fill the canvas with the background color
-      var bkColor = _utils.getComputedStyle(val, 'background-color');
+      var bkColor = utils.getComputedStyle(val, 'background-color');
       if(bkColor === 'rgba(0, 0, 0, 0)')
         bkColor = 'rgb(0,0,0)';
       context.fillStyle = bkColor;
       context.fillRect(0, 0, canvas.width, canvas.height);
 
       // Create a texture from the canvas
-      return extrovert.provider.createTextureFromCanvas( canvas, true );
+      return provider.createTextureFromCanvas( canvas, true );
     }
   };
 
 });
-
