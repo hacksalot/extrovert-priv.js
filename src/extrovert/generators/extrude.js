911b0ee8fd24b16680367eb9cac70c62f11d1460
James Devlin <james@indevious.com>
2015-05-20 20:13:33 -0400

    Tweak extrude 'depth' option.

--- src/extrovert/generators/extrude.js
+++ src/extrovert/generators/extrude.js
@@ -4,22 +4,28 @@ The built-in 'extrude' generator for Extrovert.js.
 @license Copyright (c) 2015 | James M. Devlin
 */
 
-define(['require', 'extrovert/core', 'extrovert/providers/three/provider-three'], function( require, extro, provider ) {
-
+define(
+[
+  'require',
+  'extrovert/core',
+  'extrovert/providers/three/provider-three'
+],
+function( require, extro, provider )
+{
   'use strict';
 
   var _opts, _eng, _side_mat, _noun;
-  
+
   var ExtrudeGenerator = function( ) {
-    
-    var extrovert = require('extrovert/core');    
+
+    var extrovert = require('extrovert/core');
 
     return {
 
       options: {
         name: 'extrude',
         material: { color: 0xFF8844, friction: 0.2, restitution: 1.0 },
-        block: { depth: 1 },
+        depth: 1,
         map: 'fit'
       },
 
@@ -42,7 +48,7 @@ define(['require', 'extrovert/core', 'extrovert/providers/three/provider-three']
 
       transform: function( obj ) {
         var cont = _noun.container || (_eng.opts.src && _eng.opts.src.container) || document.body;
-        return extrovert.getPosition( obj, cont, _opts.block.depth );
+        return extrovert.getPosition( obj, cont, _opts.depth );
       },
 
       rasterize: function( obj ) {
@@ -62,9 +68,9 @@ define(['require', 'extrovert/core', 'extrovert/providers/three/provider-three']
 
         var matArray;
         if( _opts.map == 'fit' ) {
-          if( !_opts.block.depth || _opts.block.depth === 'height' )
+          if( !_opts.depth || _opts.depth === 'height' )
             matArray = [ _side_mat, _side_mat, material, material, material, material ];
-          else if (_opts.block.depth === 'width' )
+          else if( _opts.depth === 'width' )
             matArray = [ material, material, _side_mat, _side_mat, material, material ];
           else
             matArray = [ _side_mat, _side_mat, _side_mat, _side_mat, material, material ];
@@ -74,7 +80,7 @@ define(['require', 'extrovert/core', 'extrovert/providers/three/provider-three']
       }
     };
   };
-  
+
   return ExtrudeGenerator;
 
 });
